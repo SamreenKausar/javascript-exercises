781603ca6f9c75f12b805bf91d4ac821acc1f05b
SamreenK <kausarsamreen28@gmail.com>
2022-05-25 13:04:14 +0400

    corrcted the issues which made it to fail all test...issue in logic '

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,14 @@
-const repeatString = function() {
-let stringValue = '';
-const num =  10;
-for(i=0; i<num; i++){
-    stringValue+= 'hey';
+const repeatString = function(stringValue, num) {
+let repeatedString ='';
+if(num>=0){
+    for(i=0; i<num; i++){
+        repeatedString +=stringValue; 
+    }
+    return repeatedString;
+}else{
+    return 'ERROR';
 }
-return stringValue;
+
 };
 
 // Do not edit below this line

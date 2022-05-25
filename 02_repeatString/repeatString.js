1fc86fac5af94e4a85cfe3542e49c608f832df06
SamreenK <kausarsamreen28@gmail.com>
2022-05-25 12:40:15 +0400

    Added logic to repeat string and Edited spec file to test all the iteration

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
 const repeatString = function() {
-
+let stringValue = '';
+const num =  10;
+for(i=0; i<num; i++){
+    stringValue+= 'hey';
+}
+return stringValue;
 };
 
 // Do not edit below this line

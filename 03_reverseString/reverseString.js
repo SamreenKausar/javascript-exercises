1381428daaf859cd0ded3743d9f89976ca661946
SamreenK <kausarsamreen28@gmail.com>
2022-05-25 12:56:56 +0400

    Added logic to the the reverseString function and tested for the result

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function( stringValue) {
+    let reverseStringVal ='';
+    for(i=stringValue.length-1; i>=0 ; i--){
+        reverseStringVal = reverseStringVal + stringValue[i];
+    }
+    return reverseStringVal;
 };
 
 // Do not edit below this line

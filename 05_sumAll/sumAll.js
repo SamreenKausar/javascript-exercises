23c7c46d287870bccb63a19d5e09582be23bb30d
SamreenK <kausarsamreen28@gmail.com>
2022-05-26 10:10:59 +0400

    Added logic to test all possibilities

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+ let sum=0;
+ if(typeof num1==="number" &&  typeof num2 ==="number"){
+    if(num1<0 || num2<0 )
+     {
+         sum ='ERROR';
+     }else if(num2<num1){
+        for(i=num2; i<= num1; i++){
+            sum+=i;
+        } 
+     }else if(num1<num2){
+        for(i=num1; i<= num2; i++){
+            sum+=i;
+        }  
+     }
+ }else{
+     sum = 'ERROR';
+ }
+ return sum;
 };
 
 // Do not edit below this line

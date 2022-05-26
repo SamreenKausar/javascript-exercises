d5dc45e53ec769721bdee454b98f2806dc38d124
SamreenK <kausarsamreen28@gmail.com>
2022-05-26 11:14:09 +0400

    Added Logic to the function and rounded off one decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+let c = ((f-32)*5)/9;
+let round = Math.round(c*10)/10;
+return round;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+ let f = ((c*9)/5) + 32;
+ let round = Math.round(f*10)/10;
+ return round;
 };
 
 // Do not edit below this line

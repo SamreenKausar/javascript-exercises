36e3bf6aedbc2a1e0b1ee987180ffa294261e2c4
SamreenK <kausarsamreen28@gmail.com>
2022-05-26 09:46:14 +0400

    Added the logic to parse the test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...theArgs) {
+    let testArray =theArgs[0];
+    let newArray =[];
+    testArray.forEach(element => {
+        if(!theArgs.includes(element)){
+            newArray.push(element);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line

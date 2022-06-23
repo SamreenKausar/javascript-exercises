0f7258c13b4e73a28ac939f8633766a88ff8e4a5
SamreenK <kausarsamreen28@gmail.com>
2022-06-23 12:14:38 +0400

    Added logic to the function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(l) {
+    let num1 =0;
+    let num2 = 1;
+    let next;
+    l = parseInt(l);
+    if(l<0){
+        return "OOPS";
+    }else{
+        for(i=1;i<=l; i++){
+            next = num1+num2;
+            num1=num2;
+            num2 =next;
+        }
+    }
+    
+return num1;
 };
+//0 next =0+1 num1=1 num2 =1
+// 1 next 1+1 num1 =1 num2 =2
+// 1 next 1+2 num1 =2 num2 = 3
+// 2
 
 // Do not edit below this line
 module.exports = fibonacci;

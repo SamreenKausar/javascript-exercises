9133e1f3781aa07dab6fa443bd825a1d7fefd0da
SamreenK <kausarsamreen28@gmail.com>
2022-06-23 11:44:14 +0400

    Added the logic for palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (stringVal) {
+ const re =/[^a-zA-Z0-9]/g;
+ const str =stringVal.toLowerCase().replace(re , '');
+ const length = str.length;
+ for(i=0; i<length/2; i++){
+    if(str[i]!=str[length-i-1]){
+        return false;
+    }
+ }
+ return true;
 };
 
 // Do not edit below this line

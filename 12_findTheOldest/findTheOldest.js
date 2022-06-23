7c7098a99dd0aa378d12708d6e078a5fcd66007e
SamreenK <kausarsamreen28@gmail.com>
2022-06-23 13:23:33 +0400

    Added sort function to find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    let oldest;
+    oldest = people.sort((a, b) =>{
+        let presentYear = new Date().getFullYear();
+        
+        const last= a.yearOfDeath==undefined ? presentYear - a.yearOfBirth :  a.yearOfDeath -a.yearOfBirth;
+        const next= b.yearOfDeath==undefined ? presentYear - b.yearOfBirth :  b.yearOfDeath -b.yearOfBirth;
+        return last>next? -1:1;
+    
+    });
+    return oldest[0];
+    };
 
 // Do not edit below this line
 module.exports = findTheOldest;
